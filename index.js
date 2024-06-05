f4924dea7a783e68061e2d2df8eac3394aec410f
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-06-06 00:39:19 +0100

    favour commit

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const firstElement = array => array[0];
+const firstQ = document.querySelectorAll(".q1");
 
-console.log(firstElement([1, 2, 3]));
+firstQ.forEach(item => {
+    item.addEventListener('click', function() {
+        let pick = this.id;
+
+        switch(pick) {
+            case "fq1":
+                console.log("1");
+                break;
+            case "fq2":
+                console.log("2");
+                break;
+            case "fq3":
+                console.log("3");
+                break;
+            case "fq4":
+                console.log("4");
+                break;
+            case "fq5":
+                console.log("5");
+                break;
+            case "fq6":
+                console.log("6");
+                break;
+            default:
+                console.log("No matching case for id:", pick);
+                break;
+        }
+    });
+});
