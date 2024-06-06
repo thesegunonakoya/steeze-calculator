0227a85666e0f02cafe57f4153a6a6bc207f70f9
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-06-06 10:03:57 +0100

    sec

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const secondQ = document.querySelectorAll(".q2");
 const thirdQ = document.querySelectorAll(".q3");
 const fourthQ = document.querySelectorAll(".q4");
 const fifthQ = document.querySelectorAll(".q5");
-const score = 100;
+let score = 100;
 let user = 0;
+let result;
 
 
 firstQ.forEach(item => {
@@ -14,30 +15,28 @@ firstQ.forEach(item => {
         switch(pick) {
             case "fq1":
                 user = user + 10;
-                console.log(user);
-                break;
+              
             case "fq2":
                 user = user + 5;
-                console.log(user);
                 break;
             case "fq3":
                 user = user + 2;
-                console.log(user);
                 break;
             case "fq4":
                 user = user + 4;
-                console.log(user);
                 break;
             case "fq5":
                 user = user + 1;
-                console.log(user);
                 break;
             case "fq6":
                 user = user - 5;
-                console.log(user);
                 break;
-            
         }
+
+      result = Number(score - user);
+      console.log(result);
+
+
     });
 });
 
@@ -103,6 +102,74 @@ thirdQ.forEach(item3 => {
                 console.log(user);
                 break;
             case "tq6":
+                user = user - 5;
+                console.log(user);
+                break;
+            
+        }
+    });
+});
+
+fourthQ.forEach(item4 => {
+    item4.addEventListener('change', function() {
+        let pick2 = this.id;
+
+        switch(pick2) {
+            case "fq1":
+                user = user + 10;
+                console.log(user);
+                break;
+            case "fq2":
+                user = user + 5;
+                console.log(user);
+                break;
+            case "fq3":
+                user = user + 2;
+                console.log(user);
+                break;
+            case "fq4":
+                user = user + 4;
+                console.log(user);
+                break;
+            case "fq5":
+                user = user + 1;
+                console.log(user);
+                break;
+            case "fq6":
+                user = user - 5;
+                console.log(user);
+                break;
+            
+        }
+    });
+});
+
+fifthQ.forEach(item5 => {
+    item5.addEventListener('change', function() {
+        let pick2 = this.id;
+
+        switch(pick2) {
+            case "fiq1":
+                user = user + 10;
+                console.log(user);
+                break;
+            case "fiq2":
+                user = user + 5;
+                console.log(user);
+                break;
+            case "fiq3":
+                user = user + 2;
+                console.log(user);
+                break;
+            case "fiq4":
+                user = user + 4;
+                console.log(user);
+                break;
+            case "fiq5":
+                user = user + 1;
+                console.log(user);
+                break;
+            case "fiq6":
                 user = user - 5;
                 console.log(user);
                 break;
